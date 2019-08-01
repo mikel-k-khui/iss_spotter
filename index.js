49a1351e66e8622c3abc5b2bc04763a0b07f408b
Mike Chui <mikel.k.khui@gmail.com>
2019-08-01 17:30:46 -0600

    Practiced more asychronous waterfall and network error handling

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
 // index.js
-const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
-const { fetchISSFlyOverTimes } = require('/iss');
+// const { fetchMyIP } = require('./iss');
+// const { fetchCoordsByIP } = require('./iss');
+// const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((ipError, ip) => {
-  // console.log(`${error} and ${ip}`);
-  if (ipError) {
-    console.log("It didn't work!" , ipError);
-    return;
-  } else {
-    console.log('It worked! Returned IP:' , ip);
-
-    fetchCoordsByIP(ip, (CoordErr, latLong) => {
-      // console.log(`${error} and ${ip}`);
-      if (CoordErr) {
-        console.log("Fetching the coordinates didn't work!" , CoordErr);
-        return;
-      } else {
-        console.log('It worked! latitude and longitude:' , latLong);
-    
-        fetchCoordsByIP(latLong, (flyErr, flyOverTime) => {
-        // console.log(`${error} and ${ip}`);
-          if (flyErr) {
-            console.log("Fetching the coordinates didn't work!" , flyErr);
-            return;
-          }
-          console.log('It worked! latitude and longitude:' , flyOverTime); 
-        });
-      }
-      //end of fetchISSFlyOverTimes(coordinates, function())
-    });
-    // end of fetchCoordsByIP(ip,function())
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
   }
+  // success, print out the deets!
+  console.log(passTimes);
 });
-//end of fetchMyIP(function())
+
+// fetchMyIP((ipError, ip) => {
+//   // console.log(`${error} and ${ip}`);
+//   if (ipError) {
+//     console.log("It didn't work!" , ipError);
+//     return;
+//   } else {
+//     console.log('It worked! Returned IP:' , ip);
+
+//     fetchCoordsByIP(ip, (CoordErr, latLong) => {
+//       // console.log(`${error} and ${ip}`);
+//       if (CoordErr) {
+//         console.log("Fetching the coordinates didn't work!" , CoordErr);
+//         return;
+//       } else {
+//         console.log('It worked! latitude and longitude:' , latLong);
+    
+//         fetchISSFlyOverTimes(latLong, (flyErr, flyOverTime) => {
+//         // console.log(`${error} and ${ip}`);
+//           if (flyErr) {
+//             console.log("Fetching the ISS Fly Over didn't work!" , flyErr);
+//             return;
+//           }
+//           console.log('It worked! You can expect to see ISS fly over at:' , flyOverTime);
+//         });
+//       }
+//       //end of fetchISSFlyOverTimes(coordinates, function())
+//     });
+//     // end of fetchCoordsByIP(ip,function())
+//   }
+// });
+// //end of fetchMyIP(function())
 
--- iss.js
+++ iss.js
@@ -14,48 +14,48 @@ const parseJSON = (requestBody) => JSON.parse(requestBody);
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
-const fetchMyIP = function(callback) {
+const fetchMyIP = function(callIpBack) {
   // use request to fetch IP address from JSON API
   searchKey.url = 'https://api.ipify.org?format=json';
-  console.log(searchKey);
+  // console.log(searchKey);
 
   request(searchKey, function(error, response, body) {
 
     if (error === null && response.statusCode === 200) {
-      callback(null,parseJSON(body)["ip"]);
+      callIpBack(null,parseJSON(body)["ip"]);
     } else if (error === null && response.statusCode !== 200) {
       const msg200 = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
       //HTTP response code only occurs iff there is no error in response
 
-      callback(Error(msg200),null);
+      callIpBack(Error(msg200),null);
     } else {
-      callback(error,null);
+      callIpBack(error,null);
     }
   });
 };
 
-const fetchCoordsByIP = function(ipAdd, callBack) {
+const fetchCoordsByIP = function(ipAdd, callCoordBack) {
   // console.log(ipAdd);
   // ipAdd = '127.0.0.1'; //comment out to test the HTTP response code
-  searchKey.url = 'https://ipvigilante.com/json/' + ipAdd;
+  searchKey.url = `https://ipvigilante.com/json/${ipAdd}`;
 
   // console.log(searchKey);
   let latLong = {};
 
   request(searchKey, function(error, response, body) {
-    console.log("Body: ", body);
+    // console.log("Body: ", body);
 
     if (error === null && response.statusCode === 200) {
-      latLong.latitude = parseJSON(body)["data"][ "latitude"];
-      latLong.longitude = parseJSON(body)["data"]["longitude"];
-      callBack(null,latLong);
+      latLong.LAT = parseJSON(body)["data"][ "latitude"];
+      latLong.LON = parseJSON(body)["data"]["longitude"];
+      callCoordBack(null,latLong);
     } else if (error === null && response.statusCode !== 200) {
       const msg200 = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
       //HTTP response code only occurs iff there is no error in response
 
-      callBack(Error(msg200),null);
+      callCoordBack(Error(msg200),null);
     } else {
-      callBack(error,null);
+      callCoordBack(error,null);
     }
   });
 };
@@ -69,11 +69,68 @@ const fetchCoordsByIP = function(ipAdd, callBack) {
  *   - The fly over times as an array of objects (null if error). Example:
  *     [ { risetime: 134564234, duration: 600 }, ... ]
  */
-const fetchISSFlyOverTimes = function(coords, callback) {
-  return(null, "hello");
+const fetchISSFlyOverTimes = function(coords, callISSBack) {
+  // coords = {"LAT": '10000000', "LON": 'afad'}; //comment out to test the HTTP response code
+  let flyOverTime = [];
+  searchKey.url = `http://api.open-notify.org/iss-pass.json?lat=${coords.LAT}&lon=${coords.LON}`;
+
+  request(searchKey, function(error, response, body) {
+    // console.log("Body: ", body);
+
+    if (error === null && response.statusCode === 200) {
+      flyOverTime = parseJSON(body)["response"];
+      callISSBack(null,flyOverTime);
+    } else if (error === null && response.statusCode !== 200) {
+      const msg200 = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      //HTTP response code only occurs iff there is no error in response
+
+      callISSBack(Error(msg200),null);
+    } else {
+      callISSBack(error,null);
+    }
+  });
+
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+/**
+ * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
+ * Input:
+ *   - A callback with an error or results.
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly-over times as an array (null if error):
+ *     [ { risetime: <number>, duration: <number> }, ... ]
+ */
+const nextISSTimesForMyLocation = function(callback) {
+
+  fetchMyIP((ipError, ip) => {
+    if (ipError) callback(ipError, null);
+    
+    fetchCoordsByIP(ip, (CoordErr, latLong) => {
+      if (CoordErr) callback(CoordErr, null);
+      
+      fetchISSFlyOverTimes(latLong, (flyErr, flyOverTime) => {
+
+        (flyErr ? callback("Fetching the ISS Fly Over didn't work!" , flyErr) : callback(null,pastTimesString(flyOverTime)));
+      });
+      //end of fetchISSFlyOverTimes(coordinates, function())
+    });
+    // end of fetchCoordsByIP(ip,function())
+  });
+  //end of fetchMyIP(function())
+};
+
+const pastTimesString = function(passTimes) {
+  let output = "";
+  for (const passTime in passTimes) {
+    let timeOfDay = new Date(0);
+    timeOfDay.setUTCSeconds(passTimes[passTime].risetime);
+    output += (`\nNext pass at ${timeOfDay} for ${passTimes[passTime].duration} seconds!\n\r`);
+  }
+  return output;
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation };
 
 /*
 $ curl 'https://api.ipify.org?format=json'
